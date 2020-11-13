0fada59e8a24a90c09874dd302968a42ad35fc33
haruaki-kuge <72724145+haruaki-kuge@users.noreply.github.com>
2020-11-13 15:33:32 +0900

    Change section 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,8 +11,13 @@ function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
+  noStroke();
+  fill(222,0,222);
+  rect(p, p, w + p * 2+p, h + p * 2+p);
+  triangle(-p*2+w/2,p*3+h,p+w/2,p*6+h,p*4+w/2,p*3+h);
+  fill(0,222,0);
   rect(0, 0, w + p * 2, h + p * 2);
+  triangle(-p*3+w/2,p*2+h,w/2,p*5+h,p*3+w/2,p*2+h);
   fill(255);
-  text(t, p, h + p);
+  text(t, p, h-p );
 }
